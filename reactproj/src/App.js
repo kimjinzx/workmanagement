e17211ceb5228d66443975cb35c812386a34ec05
kwoneungbi <rnjsdmsql7290@gmail.com>
2023-09-12 17:04:45 +0900

    컴포넌트 구성3

--- reactproj/src/App.js
+++ reactproj/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
-import "./App.css";
 import Calendar from "./components/Calendar.js";
 import Work from "./components/Work.js";
+import "./App.css";
 
 function App() {
   return (
