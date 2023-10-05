e257decf80d1f7dbb368c53592d40d8dc885c007
kwoneungbi <rnjsdmsql7290@gmail.com>
2023-10-05 14:13:22 +0900

    Calendar Component modification2

--- reactproj/src/components/Calendar.js
+++ reactproj/src/components/Calendar.js
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import koLocale from "@fullcalendar/core/locales/ko";
 import interactionPlugin from "@fullcalendar/interaction";
 import "./Calendar.css";
 import { useStore } from "./Store";
 
 const MyFullCalendar = () => {
-  const { selectedDate, setSelectedDate, prevDateEl, setPrevDateEl } =
-    useStore();
+  const { setSelectedDate, prevDateEl, setPrevDateEl } = useStore();
 
   const handleDateClick = (info) => {
     if (prevDateEl) {
@@ -21,20 +19,6 @@ const MyFullCalendar = () => {
     setSelectedDate(info.dateStr);
   };
 
-  // const allToDo = 10;
-  // const finished = 10;
-  // const count = allToDo - finished;
-  // const theDay = "2023-09-01";
-
-  // const works = [
-  //   {
-  //     classNames: ["custom-event"],
-  //     title: count,
-  //     date: theDay,
-  //     backgroundColor: "#FFFFFF",
-  //   },
-  // ];
-
   return (
     <div className="calendar-container">
       <h1>Calendar</h1>
@@ -43,23 +27,8 @@ const MyFullCalendar = () => {
           <FullCalendar
             plugins={[dayGridPlugin, interactionPlugin]}
             initialView="dayGridMonth"
-            // events={works}
-            locale={koLocale}
+            locale="ko-KR"
             dateClick={handleDateClick}
-            Add
-            this
-            prop
-            // eventContent={(arg) => {
-            // if (count === 0) {
-            //   return {
-            //     html: `<span style="color:#4374D9">✓</span>`,
-            //   };
-            // } else {
-            //   return {
-            //     html: `<span style="color:#4374D9">${count}</span>`,
-            //   };
-            // }
-            // }}
           />
         </div>
       </div>
